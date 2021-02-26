ef930532f13dde8492b908a4febc9ea2642def68
Emirhan Kenter <emirhankenter@gmail.com>
2021-02-27 01:36:20 +0300

    canvas fix

--- Assets/Game/Scripts/View/Panels/GameOverPanel.cs
+++ Assets/Game/Scripts/View/Panels/GameOverPanel.cs
@@ -3,12 +3,14 @@ using Game.Scripts.View.Elements;
 using MekNavigation;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Game.Scripts.View.Panels
 {
     public class GameOverPanel : Panel
     {
         [SerializeField] private CurrencyElement _coinElement;
+        [SerializeField] private Button _claimButton;
 
         private GameOverViewParams _params;
 
@@ -33,17 +35,18 @@ namespace Assets.Game.Scripts.View.Panels
         private void InitializeElements()
         {
             _coinElement.Init(PlayerData.Instance.Coin);
+            _claimButton.interactable = true;
             Debug.Log($"IsSuccess: {_params.IsSuccess}, Earning: {_params.EarnAmount}");
         }
 
         private void DisposeElements()
         {
-
         }
 
         [Button]
         public void OnClaimButtonClicked()
         {
+            _claimButton.interactable = false;
             Debug.Log($"Current: {PlayerData.Instance.Coin}, After: {PlayerData.Instance.Coin + _params.EarnAmount}");
             _coinElement.UpdateValue(PlayerData.Instance.Coin);
             _params.RewardClaimed?.Invoke();
